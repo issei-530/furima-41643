f9e7f7bcf954d9d18fbc98e97afb20571de62432
issei-530 <593210oi@gmail.com>
2025-01-17 21:17:38 +0900

    Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).marge(user_id: current_user.id)
+  end
 end
 
 
